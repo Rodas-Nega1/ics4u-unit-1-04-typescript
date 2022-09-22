74a94bc7af0c5019aca3bf056a746fed65bcb168
Rodas-Nega1 <Rodas-Nega1@users.noreply.github.com>
2022-09-22 15:57:04 +0000

    Prettified Code!

--- src/index.ts
+++ src/index.ts
@@ -6,30 +6,30 @@
  * Since:   2022-09-21
  */
 
-import promptSync from 'prompt-sync'
-import random from 'random'
+import promptSync from "prompt-sync";
+import random from "random";
 
 // Th
-const prompt = promptSync()
-let attempts = 1
-const dice = random.int(1, 6)
+const prompt = promptSync();
+let attempts = 1;
+const dice = random.int(1, 6);
 // input & process & output
 while (true) {
-  const numberInput = prompt('Pick a number (1-6): ')
-  const player = parseFloat(numberInput)
+  const numberInput = prompt("Pick a number (1-6): ");
+  const player = parseFloat(numberInput);
   if (player > 6 || player < 1) {
-    console.log('\nThat is an invalid number.')
+    console.log("\nThat is an invalid number.");
   } else if (player === dice) {
-    console.log('\nYou guessed right!')
-    console.log(`\nIt took you ${attempts} tries.`)
-    break
+    console.log("\nYou guessed right!");
+    console.log(`\nIt took you ${attempts} tries.`);
+    break;
   } else if (player > dice) {
-    console.log('\nYou guessed too high, try again.')
+    console.log("\nYou guessed too high, try again.");
   } else if (player < dice) {
-    console.log('\nYou guessed too low, try again')
+    console.log("\nYou guessed too low, try again");
   } else if (isNaN(player)) {
-    console.log('\nThat is an invalid input.')
+    console.log("\nThat is an invalid input.");
   }
-  attempts += 1
+  attempts += 1;
 }
-console.log('\nDone.')
+console.log("\nDone.");
